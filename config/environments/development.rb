bca3103ad200fe5dc791f39e09af7cbb7e0aab78
Ebizuka <a2005me@aiit.ac.jp>
2021-05-28 09:14:07 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,12 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  host = 'https://01c683651cce4b52af4257ef4e64a37f.vfs.cloud9.ap-northeast-1.amazonaws.com' # 自分の環境のホストに変えてください。
+  # クラウドIDEの場合は以下をお使いください
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  # localhostで開発している場合は以下をお使いください
+  # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   config.action_mailer.perform_caching = false
 
